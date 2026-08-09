195246cf9be4c6c0fabcd09e8a968bba05b29824
Hariharasudhan2306 <harisudhan7562@gmail.com>
2026-08-09 18:16:01 +0530

    Change GitHub Pages path to lowercase

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ import react from '@vitejs/plugin-react-swc'
 // })
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 })
